a7e949ff3b980ec5a809c20c791ebb6340ac71be
Nannaglawnielsen <nan002@edu.zealand.dk>
2024-05-15 10:44:13 +0200

    db

--- settings/init.php
+++ settings/init.php
@@ -8,10 +8,10 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED & ~E_STRICT);
 define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
 if(CONFIG_LIVE == 0){
-    $DB_SERVER = "localhost";
-    $DB_NAME = "test";
-    $DB_USER = "root";
-    $DB_PASS = "root";
+    $DB_SERVER = "mysql5.unoeuro.com";
+    $DB_NAME = "glaw_dk_db_e2Strandparken";
+    $DB_USER = "glaw_dk";
+    $DB_PASS = "ramgcyekdt93RfzwDAE6";
 }else{
     $DB_SERVER = "";
     $DB_NAME = "";
