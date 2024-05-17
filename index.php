b943df558a1106ad6b9bf799bada754231168b8d
Jonatan Emil Rytter <jor001@edu.zealand.dk>
2024-05-17 13:25:13 +0200

    firkant arbejde. Ignore init fil

--- index.php
+++ index.php
@@ -17,41 +17,52 @@ require "settings/init.php";
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body>
+<body class="bg-baggrund">
 <div class="container">
-<div class="row d-flex align-items-center">
-    <div class="col-6">
-        <div>
-            <div class="col-12 fs-1 text-center">
-                <span id="todayTemp">0</span><span>&#8451;</span>
-            </div>
-            <div class="col-12 fs-1 text-center">
-                <div id="clock"></div>
+    <div class="row d-flex align-items-center">
+        <div class="col-6">
+            <div>
+                <div class="col-12 fs-1 text-center">
+                    <span id="todayTemp">0</span><span>&#8451;</span>
+                </div>
+                <div class="col-12 fs-1 text-center">
+                    <div id="clock"></div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-6">
-        <div class="row g-2">
+        <div class="col-6">
+
+            <div class="col-4">
+                <div class="square bg-danger">
+                    <div class="sqcontent">
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+            <div class="row g-2">
             <?php
             $produkter = $db->sql("SELECT * FROM diamanter");
-            foreach ($produkter as $produkt) {
-                ?>
-                <div class="col-12 col-md-6">
-                    <div class="card w-100">
-                        <div class="card-header">
-                            <?php
+             foreach ($produkter as $produkt) {
+                 ?>
+                 <div class="col-4">
+                     <div class="card w-100">
+                         <div class="card-header">
+                             <?php
                             echo $produkt->diamantNavn;
-                            ?>
+            ?>
                         </div>
                         <div class="card-body">
                             <?php
-                            // Indsæt andet felt fra database
-                            ?>
+            // Indsæt andet felt fra database
+            ?>
                         </div>
                         <div class="card-footer text-muted">
                             <?php
-                            // Indsæt andet felt fra database
-                            ?>
+            // Indsæt andet felt fra database
+            ?>
                         </div>
                     </div>
                 </div>
@@ -60,8 +71,7 @@ require "settings/init.php";
             ?>
         </div>
 
-    </div>
-</div>
+        </div>
 </div>
 
 
