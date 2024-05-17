c292a97c35cd6de6667c63d72c9c336262ccfd45
Nannaglawnielsen <nan002@edu.zealand.dk>
2024-05-17 13:07:29 +0200

    navbar

--- navbar.php
+++ navbar.php
@@ -1,8 +1,15 @@
 <link href="css/styles.css" rel="stylesheet" type="text/css">
 
-<nav class="navbar bg-body-tertiary">
-    <form class="container-fluid justify-content-start">
-        <button class="btn btn-outline-success me-2" type="button">Main button</button>
+<nav class="navbar bg-baggrund">
+    <div class="d-flex m-3">
+        <button class="btn btn-outline-success me-2" type="button"><i class="fa-solid fa-arrow-left"></i>
+        </button>
+    </div>
+
+        <div class="d-flex justify-content-end m-3">
         <button class="btn btn-sm btn-outline-secondary" type="button">Smaller button</button>
-    </form>
+        </div>
+
 </nav>
+
+<script src="https://kit.fontawesome.com/056ca3e665.js" crossorigin="anonymous"></script>
