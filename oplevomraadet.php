dcfe72e7fae720ef0067b83d90fd9abfc47876fa
amalie <amp001@edu.zealand.dk>
2024-05-21 14:12:41 +0200

    Oplev området

---
+++ oplevomraadet.php
@@ -0,0 +1,102 @@
+<?php
+require "settings/init.php";
+?>
+    <!DOCTYPE html>
+    <html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Oplev området</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body class="bg-baggrund text-text">
+<?php include "navbar.php"; ?>
+
+<br>
+<div class="container">
+    <div class="row">
+        <div class="row pb-4 border-bottom border-2 border-hvid" >
+            <div class="col ps-5">
+                <img class="rounded-2" src="img/vinsmagning.webp">
+            </div>
+            <div class="col">
+                <div class="pt-4">
+                    <p class="fs-1 overskrift fw-medium">Vinsmagning</p>
+                    <p>Træd ind i vinens verden og udforsk en mangfoldighed af <br> smage og aromaer med vores eksklusive
+                        vinsmagning.
+                        <br>Perfekt for både nybegyndere og vinentusiaster!</p>
+                    <p class="fst-italic">Tidspunkt: d. 12/07 kl 20 i Restaurant Blossom </p>
+                    <p>Tilmeldelse via vores hjemmeside</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br>
+    <div class="container">
+        <div class="row">
+            <div class="row pb-4 border-bottom border-2 border-hvid">
+                <div class="col">
+                    <div class=" ps-3 pt-2">
+                        <p class="fs-1 overskrift fw-medium"> Flow Yoga</p>
+                        <p class="fs-5">For nybegyndere</p>
+                        <p>Find ro, styrke og fleksibilitet med vores Flow Yoga for begyndere. <br>
+                            Perfekt for dig, der vil starte yogarejsen i en støttende og afslappet atmosfære.
+                            Kontakt personale for mere info.</p>
+                        <p class="fst-italic">Tidspunkt: d. 14/05 kl 10. Varighed 60 min</p>
+                        <p>Du er velkommen til at medbringe din egen måtte.</p>
+                    </div>
+                </div>
+                <div class="col ps-5">
+                    <img class="rounded-2" src="img/yoga.webp">
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="container">
+            <div class="row">
+                <div class="row pb-4 border-bottom border-2 border-hvid">
+                    <div class="col ps-4">
+                        <img class="rounded-2" src="img/madsmagning.webp">
+                    </div>
+                    <div class="col">
+                        <div class="ps-3 pt-4">
+                            <p class="fs-1 overskrift fw-medium">Restaurant Blossom </p>
+                            <p class="fs-5">Tester ny menu</p>
+                            <p>Kom og smag vores spændende nye menu hos Restaurant<br> Blossom!
+                                Vi har nøje udvalgt friske ingredienser og innovative </br>retter, der vil forkæle dine
+                                smagsløg.</p>
+                            <p class="fst-italic">Prøv nu. Book bord fra kl 17.30 hver dag.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br><br>
+            <div class="container">
+                <div class="row">
+                    <div class="col pt-4">
+                        <p class="fs-1 overskrift fw-medium ">Valentines dag</p>
+                        <p>Gør Valentinsdag til en uforglemmelig begivenhed med et <br>
+                            ophold på Hotel Strandparken. Book jeres romantiske getaway <br>
+                            i dag og lad os sørge for, at I fejrer kærligheden på bedste vis.</p>
+                        <p>Giv en oplevelse din partner aldrig vil glemme. Mere infomation <br>
+                            findes på vores hjemmeside.</p>
+                        <p class="fst-italic">Tidspunkt. d. 14-16/02 - Valentinesweekend</p>
+                    </div>
+                    <div class="col ps-5">
+                        <img class="rounded-2" src="img/valentines.webp">
+                    </div>
+                </div>
+                <br>
+
+
+                <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+<?php
+
