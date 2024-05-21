d1685dd6a6e0d72ae4a1a5ee10ce3a0bba064c43
Nannaglawnielsen <nan002@edu.zealand.dk>
2024-05-21 10:42:00 +0200

    navbar

--- navbar.php
+++ navbar.php
@@ -1,16 +1,16 @@
 <link href="css/styles.css" rel="stylesheet" type="text/css">
 
 <nav class="navbar bg-navbar">
-    <div class="d-flex m-3">
+    <div class="d-flex m-2">
         <button class="btn me-2" type="button"><i class="pil fa-solid fa-arrow-left "></i>
         </button>
     </div>
 
     <div class="pt-4 ps-4">
-        <p class="navover">BOOK GOLF</p>
+        <p class="navover">Book golf</p>
     </div>
 
-        <div class="d-flex justify-content-end m-3">
+        <div class="d-flex justify-content-end m-2">
         <button class="btn btn-sm" type="button"><i class="sprog fa-solid fa-globe"></i></button>
         <button class="btn btn-sm" type="button"><i class="info fa-solid fa-circle-info"></i></button>
         <button class="btn btn-sm" type="button"><i class="FAQ fa-solid fa-circle-question"></i></button>
