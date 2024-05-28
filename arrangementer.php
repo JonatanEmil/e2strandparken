70e2df4ca74a8573c1ab8792459dfe397502c12d
Nannaglawnielsen <nan002@edu.zealand.dk>
2024-05-28 10:28:45 +0200

    arrangementer og oplev

--- arrangementer.php
+++ arrangementer.php
@@ -21,12 +21,11 @@ require "settings/init.php";
 
 <br>
 <div class="container">
-    <div class="row">
-        <div class="row pb-4 border-bottom border-2 border-kasse" >
-        <div class="col ps-5">
+    <div class="row pb-4 border-bottom border-2 border-kasse">
+        <div class="col-6 ps-0">
             <img class="rounded-2" src="img/vinsmagning.webp" alt="På billedet er der folk, som er til vinsmagning">
         </div>
-        <div class="col">
+        <div class="col-6">
             <div class="pt-4">
                 <p class="fs-1 overskrift fw-medium">Vinsmagning</p>
                 <p>Træd ind i vinens verden og udforsk en mangfoldighed af <br> smage og aromaer med vores eksklusive
@@ -37,65 +36,61 @@ require "settings/init.php";
             </div>
         </div>
     </div>
+
+    <br><br>
+    <div class="row pb-4 border-bottom border-2 border-kasse">
+        <div class="col-6">
+            <div class=" ps-0 pt-2">
+                <p class="fs-1 overskrift fw-medium"> Flow Yoga</p>
+                <p class="fs-5">For nybegyndere</p>
+                <p>Find ro, styrke og fleksibilitet med vores Flow Yoga for begyndere. <br>
+                    Perfekt for dig, der vil starte yogarejsen i en støttende og afslappet atmosfære.
+                    Kontakt personale for mere info.</p>
+                <p class="fst-italic">Tidspunkt: d. 14/05 kl 10. Varighed 60 min</p>
+                <p>Du er velkommen til at medbringe din egen måtte.</p>
+            </div>
+        </div>
+        <div class="col-6 d-flex justify-content-end">
+            <img class="img-fluid rounded-2" src="img/yoga.webp" alt="Billede af folk der dyrker yoga">
+        </div>
     </div>
     <br><br>
-    <div class="container">
-        <div class="row">
-            <div class="row pb-4 border-bottom border-2 border-kasse">
-            <div class="col-6">
-                <div class=" ps-3 pt-2">
-                    <p class="fs-1 overskrift fw-medium"> Flow Yoga</p>
-                    <p class="fs-5">For nybegyndere</p>
-                    <p>Find ro, styrke og fleksibilitet med vores Flow Yoga for begyndere. <br>
-                        Perfekt for dig, der vil starte yogarejsen i en støttende og afslappet atmosfære.
-                        Kontakt personale for mere info.</p>
-                    <p class="fst-italic">Tidspunkt: d. 14/05 kl 10. Varighed 60 min</p>
-                    <p>Du er velkommen til at medbringe din egen måtte.</p>
-                </div>
-            </div>
-            <div class="col-6 d-flex justify-content-end">
-                <img class="img-fluid rounded-2" src="img/yoga.webp" alt="Billede af folk der dyrker yoga">
+
+    <div class="row pb-4 border-bottom border-2 border-kasse g-4">
+        <div class="col-6 ps-0">
+            <img class="rounded-2" src="img/madsmagning.webp" alt="Billede af folk der bestiller mad">
+        </div>
+        <div class="col-6">
+            <div class="ps-3 pt-4">
+                <p class="fs-1 overskrift fw-medium">Restaurant Blossom </p>
+                <p class="fs-5">Tester ny menu</p>
+                <p>Kom og smag vores spændende nye menu hos Restaurant<br> Blossom!
+                    Vi har nøje udvalgt friske ingredienser og innovative </br>retter, der vil forkæle dine
+                    smagsløg.</p>
+                <p class="fst-italic">Prøv nu. Book bord fra kl 17.30 hver dag.</p>
             </div>
         </div>
+    </div>
+
+    <br><br>
+    <div class="row">
+        <div class="col-6 pt-4">
+            <p class="fs-1 overskrift fw-medium ">Valentines dag</p>
+            <p>Gør Valentinsdag til en uforglemmelig begivenhed med et
+                ophold på Hotel Strandparken. Book jeres romantiske getaway
+                i dag og lad os sørge for, at I fejrer kærligheden på bedste vis.</p>
+            <p>Giv en oplevelse din partner aldrig vil glemme. Mere infomation <br>
+                findes på vores hjemmeside.</p>
+            <p class="fst-italic">Tidspunkt. d. 14-16/02 - Valentinesweekend</p>
         </div>
-        <br><br>
-        <div class="container">
-            <div class="row">
-                <div class="row pb-4 border-bottom border-2 border-kasse">
-                    <div class="col ps-4">
-                        <img class="rounded-2" src="img/madsmagning.webp" alt="Billede af folk der bestiller mad">
-                    </div>
-                    <div class="col">
-                        <div class="ps-3 pt-4">
-                            <p class="fs-1 overskrift fw-medium">Restaurant Blossom </p>
-                            <p class="fs-5">Tester ny menu</p>
-                            <p>Kom og smag vores spændende nye menu hos Restaurant<br> Blossom!
-                                Vi har nøje udvalgt friske ingredienser og innovative </br>retter, der vil forkæle dine
-                                smagsløg.</p>
-                            <p class="fst-italic">Prøv nu. Book bord fra kl 17.30 hver dag.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <br><br>
-            <div class="container">
-                <div class="row">
-                    <div class="col-6 pt-4">
-                        <p class="fs-1 overskrift fw-medium ">Valentines dag</p>
-                        <p>Gør Valentinsdag til en uforglemmelig begivenhed med et
-                            ophold på Hotel Strandparken. Book jeres romantiske getaway
-                            i dag og lad os sørge for, at I fejrer kærligheden på bedste vis.</p>
-                        <p>Giv en oplevelse din partner aldrig vil glemme. Mere infomation <br>
-                            findes på vores hjemmeside.</p>
-                        <p class="fst-italic">Tidspunkt. d. 14-16/02 - Valentinesweekend</p>
-                    </div>
-                    <div class="col d-flex justify-content-end">
-                        <img class="rounded-2 img-fluid" src="img/valentines.webp" alt="Billede af blomster og gaver">
-                    </div>
-                </div>
-                <br>
+        <div class="col-6 d-flex justify-content-end">
+            <img class="rounded-2" src="img/valentines.webp" alt="Billede af blomster og gaver">
+        </div>
+    </div>
+    <br>
+</div>
 
 
-                <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 <?php
